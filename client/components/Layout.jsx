77c9f848f9671a0bd4248f96181c2e1a021b3f86
mechbrian <mechbrian062@gmail.com>
2025-01-26 15:07:17 -0500

    modify header

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Layout.css";
 
-export default function Layout(){
-    return(
-        <>
-        <h1>My Portfolio</h1>
-        <nav>
-            <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/education">Education</Link> | <Link to="/project">Porject</Link> | <Link to="/contact">Contact</Link>
-        </nav>
-        <hr />            
-        </>
-    );
+export default function Layout() {
+  return (
+    <header className="layout-header">
+      <div className="logo">
+        <Link to="/">
+          <img src="../src/assets/logo.png" alt="logo" />
+        </Link>
+      </div>
+      <nav className="navigation">
+        <Link to="/" className="nav-button">
+          Home
+        </Link>{" "}
+        |
+        <Link to="/about" className="nav-button">
+          About
+        </Link>{" "}
+        |{" "}
+        <Link to="/education" className="nav-button">
+          Education
+        </Link>{" "}
+        |{" "}
+        <Link to="/project" className="nav-button">
+          Porject
+        </Link>{" "}
+        |
+        <Link to="/contact" className="nav-button">
+          Contact
+        </Link>
+      </nav>
+      <br />
+      <hr />
+    </header>
+  );
 }
